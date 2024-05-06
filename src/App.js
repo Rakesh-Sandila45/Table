68f881e95f6f2f1113c7725f49f5bffd4d993995
rakesh45 <rakeshyadav970535@gmail.com>
2024-05-06 14:49:31 +0530

    configured

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import "./App.css";
-import Table from "./components/table";
-function App() {
+import React from "react";
+import ReactDOM from "react-dom";
+import { loadRemoteComponent } from "module-federation";
+
+const App = () => {
+  const TileComponent = React.lazy(() =>
+    loadRemoteComponent({
+      remote: "tileApp",
+      component: "./tile", // Path to the Tile component within the Tile app
+    })
+  );
+
   return (
-    <div className="App">
-      <Table />
+    <div>
+      <h1>Table App</h1>
+      <React.Suspense fallback="Loading TileComponent...">
+        <TileComponent />
+      </React.Suspense>
     </div>
   );
-}
+};
 
-export default App;
+ReactDOM.render(<App />, document.getElementById("root"));
