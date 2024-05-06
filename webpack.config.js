40db4564fde207d272086a82a372fcd35f872d55
rakesh45 <rakeshyadav970535@gmail.com>
2024-05-06 15:15:46 +0530

    build

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,9 @@ const { ModuleFederationPlugin } = require("webpack").container;
 module.exports = {
   plugins: [
     new ModuleFederationPlugin({
-      name: "hostApp",
-      filename: "remoteEntry.js", // File name for remote entry manifest
-      exposes: {
-        "./table": "./src/components/table", // Expose MyComponent
+      name: "remoteApp",
+      remotes: {
+        hostApp: "https://main--snazzy-valkyrie-328f8e.netlify.app/", // Remote entry URL of host application
       },
       shared: ["react", "react-dom"], // Share React and React DOM
     }),
