50db30d86ae634e7cccec50368daacfa335d93f6
rakesh45 <rakeshyadav970535@gmail.com>
2024-05-06 15:11:42 +0530

    build

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,12 @@
 const { ModuleFederationPlugin } = require("webpack").container;
-const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath: "http://path-to-table-app.com/", // Update with your Table app's public URL
-  },
   plugins: [
     new ModuleFederationPlugin({
-      name: "tableApp",
-      remotes: {
-        tileApp: "tileApp@http://path-to-tile-app.com/remoteEntry.js", // Remote entry URL of Tile app
+      name: "hostApp",
+      filename: "remoteEntry.js", // File name for remote entry manifest
+      exposes: {
+        "./table": "./src/components/table", // Expose MyComponent
       },
       shared: ["react", "react-dom"], // Share React and React DOM
     }),
